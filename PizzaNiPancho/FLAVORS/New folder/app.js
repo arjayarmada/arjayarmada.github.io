2f25fee5f009513accac34acd96b12fbc02f68f9
arjayarmada <133829194+arjayarmada@users.noreply.github.com>
2023-05-17 10:13:15 +0800

    Add files via upload

---
+++ PizzaNiPancho/FLAVORS/New folder/app.js
@@ -0,0 +1,14 @@
+function gotoLink(link){
+    console.log(link.value);
+    location.href = 'file:///C:/Users/Arjay/Desktop/PizzaNiPancho/FLAVORS/BONE.html';
+};
+
+function myFunction1() {
+    console.log(link.value);
+    alert('ORDER COMPLETE THANK YOU');
+    location.href ='file:///C:/Users/Arjay/Desktop/PizzaNiPancho/FLAVORS/BONE.html';
+  }
+
+  function showAlert() {
+    alert ("Hello world!");
+  }
---
+++ PizzaNiPancho/FLAVORS/app5.js
@@ -0,0 +1,10 @@
+function gotoLink(link){
+    console.log(link.value);
+    location.href = 'file:///C:/Users/Arjay/Desktop/PizzaNiPancho/FLAVORS/BONE.html';
+};
+
+
+function alerto(){
+  alert('ORDER COMPLETE THANKYOU');
+  location.href = 'file:///C:/Users/Arjay/Desktop/PizzaNiPancho/FLAVORS/BONE.html';
+}
