36d44be878c5778ca364d943ffcb4d8dd0711410
arjayarmada <133829194+arjayarmada@users.noreply.github.com>
2023-05-27 20:04:54 +0800

    Add files via upload

---
+++ HCIPROJECT/app4.js
@@ -0,0 +1,10 @@
+function gotoLink(link){
+  console.log(link.value);
+  location.href = './BONE.html';
+};
+
+
+function alerto(){
+alert('ORDER COMPLETE THANKYOU');
+location.href = './index.html';
+}
